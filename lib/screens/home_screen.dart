71c6815f6b4ddee1e1e4c36b5fb7151fde4a2c55
NelsonSaputraEdika <nelsonsaputraedika_2226250034@mhs.mdp.ac.id>
2025-04-27 23:11:46 +0700

    po

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -39,7 +39,7 @@ class HomeScreen extends StatelessWidget {
                           ),
                         ),
                         Text(
-                          'Owner',
+                          'popo',
                           style: TextStyle(
                             fontSize: 16,
                             color: Colors.grey,
